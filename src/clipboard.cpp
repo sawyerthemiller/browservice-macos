ebeed2f0f03db2350a633b75ef90d8ea2d1abe9d
Sawyer Miller <111768633+sawyerthemiller@users.noreply.github.com>
2026-03-27 12:58:13 -0500

    cleaning up

--- src/clipboard.cpp
+++ src/clipboard.cpp
@@ -9,10 +9,6 @@ extern "C" void cef_chromiumBrowserviceClipboardCopy(const char* str);
 
 void copyToClipboard(string str) {
 #ifdef USE_STD_CEF
-	// When using standard CEF, we cannot access the internal clipboard functions.
-    // For now, we will just warn or do nothing, as implementing full clipboard integration
-    // without the patches requires a different approach (e.g. platform specific clipboard API).
-    // TODO: Implement native clipboard support for macOS/Linux-std-cef
     WARNING_LOG("Clipboard copy not supported with standard CEF");
 #else
 	cef_chromiumBrowserviceClipboardCopy(str.c_str());
